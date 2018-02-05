32e30a00ea36f00e7fed08318e8e420836e7b321
Firzinat.Khuzeev <firzinat.khuzeev@outlook.com>
2018-02-05 16:01:05 +0300

    1

--- deployment/post-install.sh
+++ deployment/post-install.sh
@@ -22,18 +22,21 @@ sudo /etc/init.d/xrdp start
 sudo service xrdp restart
 
 # Install CUDA Drivers
-# CUDA_REPO_PKG=cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-# wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
-# sudo dpkg -i /tmp/${CUDA_REPO_PKG}
-# sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
-# rm -f /tmp/${CUDA_REPO_PKG}
-# sudo apt-get update -y
-# sudo apt-get install cuda-drivers -y
+CUDA_REPO_PKG=cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
+sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
+rm -f /tmp/${CUDA_REPO_PKG}
+sudo apt-get update -y
+sudo apt-get install cuda-drivers -y
 
-# # Install CUDA toolkit
-# sudo apt-get install cuda-8-0 cuda-toolkit-8-0 -y
+# Install CUDA toolkit
+sudo apt-get install cuda-8-0 cuda-toolkit-8-0 -y
 
 # Install Blender
 sudo apt-get install blender -y
 
-# sudo reboot
+# Download blender example
+wget https://download.blender.org/demo/test/BMW27_2.blend.zip
+sudo apt-get install unzip -y
+unzip BMW27_2.blend.zip
