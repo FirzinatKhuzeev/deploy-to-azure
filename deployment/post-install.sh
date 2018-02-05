fd3020251e0f1fdaaf8c4673d059659d800bdaa5
Firzinat.Khuzeev <firzinat.khuzeev@outlook.com>
2018-02-05 14:34:26 +0300

    remove drivers

--- deployment/post-install.sh
+++ deployment/post-install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-#update
-sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
+# Remove lock
+# rm /var/lib/dpkg/lock
+# dpkg --configure -a
+
+# Update
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
 
 # Install xRDP
 sudo apt-get install xrdp -y
@@ -16,15 +22,16 @@ sudo /etc/init.d/xrdp start
 sudo service xrdp restart
 
 # Install CUDA Drivers
-CUDA_REPO_PKG=cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
-sudo dpkg -i /tmp/${CUDA_REPO_PKG}
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
-rm -f /tmp/${CUDA_REPO_PKG}
-sudo apt-get update -y
-sudo apt-get install cuda-drivers -y
-# Install CUDA toolkit
-sudo apt-get install cuda-8-0 cuda-toolkit-8-0 -y
+# CUDA_REPO_PKG=cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+# wget -O /tmp/${CUDA_REPO_PKG} http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/${CUDA_REPO_PKG} 
+# sudo dpkg -i /tmp/${CUDA_REPO_PKG}
+# sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub 
+# rm -f /tmp/${CUDA_REPO_PKG}
+# sudo apt-get update -y
+# sudo apt-get install cuda-drivers -y
+
+# # Install CUDA toolkit
+# sudo apt-get install cuda-8-0 cuda-toolkit-8-0 -y
 
 # Install Blender
 sudo apt-get install blender -y
