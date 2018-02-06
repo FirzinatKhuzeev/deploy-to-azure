793f76778d91da729be46d69ea198fedc532ad8d
Firzinat Khuzeev <firzinat.khuzeev@akvelon.com>
2018-02-06 15:20:21 +0300

    added wait

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
-# deploy-to-azure
-
-
-Deply Data Science Virtual Machine for Linux (Ubuntu)
+# Deploy to Azure
 
 [![Deploy to Azure](https://azuredeploy.net/deploybutton.svg)](https://deploy.azure.com/?repository=https://github.com/huzferd/deploy-to-azure)
 
-<a href="http://armviz.io/#/?load=https://deploy.azure.com/?repository=https://github.com/huzferd/deploy-to-azure?ptmpl=azuredeploy.json" target="_blank">
-<img src="http://armviz.io/visualizebutton.png"/>
+<a href="http://armviz.io/#/?load=https://raw.githubusercontent.com/huzferd/deploy-to-azure/master/azuredeploy.json" target="_blank">
+    <img src="http://armviz.io/visualizebutton.png"/>
+</a>
--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,6 +3,25 @@
 echo "starting install on pid $$"
 date
 
+if (fuser /var/lib/dpkg/lock > /dev/null 2>&1)
+    then echo "/var/lib/dpkg/lock is locked"
+fi
+
+i=0
+while (fuser /var/lib/dpkg/lock > /dev/null 2>&1); do
+    sleep 30
+    ((i=i+1))
+    if ((i > 60)) then
+        echo "    Waiting failed after 30m."
+        ps axf | grep apt | grep -v grep | grep -v waitForApt | awk '{print "sudo kill -9 " $1}' | sh
+        echo "    Cleaning up..."
+        sudo rm /var/lib/apt/lists/lock -f
+        sudo rm /var/cache/apt/archives/lock -f
+        sudo rm /var/lib/dpkg/lock -f
+        break
+    fi
+done
+
 # Update
 sudo apt-get update -y
 sudo apt-get upgrade -y
