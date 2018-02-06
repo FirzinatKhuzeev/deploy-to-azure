9b76df74a90a5a00929d45e40d7e1d8b2adf9e8d
Firzinat Khuzeev <firzinat.khuzeev@akvelon.com>
2018-02-06 13:38:21 +0300

    template rg

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -7,9 +7,32 @@ date
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
-# Run other scripts
-sudo ./app-install.sh
-sudo ./dependencies-install.sh
+# Install xRDP
+sudo apt-get install xrdp -y
+
+# Install XFCE
+sudo apt-get install xfce4 -y
+
+# Configure XFCE
+echo xfce4-session >~/.xsession
+sudo sed -i.bak '/fi/a startxfce4\n' /etc/xrdp/startwm.sh
+sudo /etc/init.d/xrdp start
+sudo service xrdp restart
+
+# Install Blender
+sudo apt-get install blender -y
+
+# Install UnZip
+sudo apt-get install unzip -y
+
+# Download Blender example
+wget https://download.blender.org/demo/test/BMW27_2.blend.zip
+bldPath="/opt/blender-example"
+sudo mkdir $bldPath
+unzip ./BMW27_2.blend.zip
+sudo cp ./bmw27/* $bldPath
+sudo chmod -R 777 $bldPath
+sudo chown -R :users $bldPath
 
 date
 echo "completed install on pid $$"
