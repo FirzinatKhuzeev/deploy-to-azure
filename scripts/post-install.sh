eb75d59f9e791804d6fefa18841eeb08c3bd5e6c
Firzinat Khuzeev <firzinat.khuzeev@akvelon.com>
2018-02-07 10:28:08 +0300

    added logger

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-echo "Starting install on pid $$"
-date
+log() {
+    echo "[post-install] $1"
+    date
+}
 
-ps axf | grep apt | grep -v grep | grep -v waitForApt | awk '{print "sudo kill -9 " $1}' | sh
+log "Starting post install on pid $$"
+
+log "Start removing locks"
+
+ps axf | grep apt | grep -v grep | awk '{print "sudo kill -9 " $1}' | sh
 sudo rm /var/lib/apt/lists/lock -f
 sudo rm /var/cache/apt/archives/lock -f
 sudo rm /var/lib/dpkg/lock -f
@@ -11,53 +17,32 @@ sudo dpkg --configure -a
 sudo apt-get update -y
 sudo apt-get -f install -y
 
-# if (fuser /var/lib/dpkg/lock > /dev/null 2>&1)
-#     then echo "/var/lib/dpkg/lock is locked"
-# fi
+log "Completed the removal of locks"
 
-# i=0
-# while (fuser /var/lib/dpkg/lock > /dev/null 2>&1); do
-#     sleep 30
-#     ((i=i+1))
-#     if ((i > 60))
-#     then
-#         echo "Waiting failed after 30m."
-#         ps axf | grep apt | grep -v grep | grep -v waitForApt | awk '{print "sudo kill -9 " $1}' | sh
-#         echo "Cleaning up..."
-#         sudo rm /var/lib/apt/lists/lock -f
-#         sudo rm /var/cache/apt/archives/lock -f
-#         sudo rm /var/lib/dpkg/lock -f
-#         break
-#     fi
-# done
-
-echo "Updating..."
-date
-
-# Update
+log "Update package database"
 sudo apt-get update -y
 # sudo apt-get upgrade -y
 
-# Install xRDP
+log "Install xRDP"
 sudo apt-get install xrdp -y
 sudo ufw allow 3389/tcp
 
 # Install XFCE
 # sudo apt-get install xfce4 -y
 
-# Configure XFCE
+log "Configure XFCE"
 echo xfce4-session >~/.xsession
 sudo sed -i.bak '/fi/a startxfce4\n' /etc/xrdp/startwm.sh
 sudo /etc/init.d/xrdp start
 sudo service xrdp restart
 
-# Install Blender
+log "Install Blender"
 sudo apt-get install blender -y
 
-# Install UnZip
+log "Install UnZip"
 sudo apt-get install unzip -y
 
-# Download Blender example
+log "Download Blender example"
 wget https://download.blender.org/demo/test/BMW27_2.blend.zip
 bldPath="/opt/blender-example"
 sudo mkdir $bldPath
@@ -66,9 +51,8 @@ sudo cp ./bmw27/* $bldPath
 sudo chmod -R 777 $bldPath
 sudo chown -R :users $bldPath
 
-# Restart services
+log "Restart services"
 sudo service xrdp restart
 sudo service ssh restart
 
-date
-echo "Completed install on pid $$"
+log "Completed post install on pid $$"
